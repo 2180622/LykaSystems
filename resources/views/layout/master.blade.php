ec409443e7f58e158dcbf66cd8d3c060a31b750f
José Areia <jose.apareia@gmail.com>
2020-03-06 16:50:45 +0000

    File Structure - *UPDATE*

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title') - Lyka Systems</title>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
