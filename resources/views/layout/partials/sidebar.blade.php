15c6509c74b31fb95a65dd3d1b18abe89879c1e0
Daniel <daniel-nunes@live.com>
2020-03-09 15:15:20 +0000

    Notifications

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -1,71 +1,112 @@
 <div class="mx-auto mb-3 pt-3 font-weight-bold text-center" style="color:#6A74C9;font-size:18px">
-  <i class="fas fa-bell mr-2"></i>Notificações<br>
+    <i class="fas fa-bell mr-2"></i>Notificações<br>
 </div>
 
 
 <div class="container-fluid " style="font-size:14px; height:70%">
 
-<div class="text-muted text-center m-2" style="font-size:12px">6 de Março de 2020</div>  
+    <div class="text-muted text-center m-2" style="font-size:12px">6 de Março de 2020</div>
 
-  {{-- Exemplo de notificação --}}
-  <div class="row p-1 mx-1 alert alert-danger" >
+
+
+    {{-- Exemplo de notificação --}}
+    <div class="alert-dismissible row p-1 mx-1 alert alert-danger fade show">
+
+        {{-- Exemplo de "marcar como lido" --}}
+        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
+                    title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+
+        <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
+            {{-- ICON da notificação --}}
+            <i class="fas fa-euro-sign "></i>
+        </div>
+
+        <div class="col p-2 ">
+            {{-- MENSAGEM da notificação --}}
+            Existem pagamentos em atraso. <a href="#">Ver lista</a>
+        </div>
+    </div>
+
+
+
+
+
+    {{-- Exemplo de notificação --}}
+    <div class="alert-dismissible row p-1 mx-1 alert alert-success fade show">
+
+        {{-- Exemplo de "marcar como lido" --}}
+        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
+                    title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+
+        <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
+            {{-- ICON da notificação --}}
+            <i class="fas fa-university"></i>
+        </div>
+
+        <div class="col p-2 ">
+            {{-- MENSAGEM da notificação --}}
+            UBI: Candidaturas abertas <a href="#">Ver website</a>
+        </div>
+    </div>
+
+
+
+
+    {{-- Exemplo de notificação --}}
+    <div class="alert-dismissible row p-1 mx-1 alert alert-info fade show">
+
+      {{-- Exemplo de "marcar como lido" --}}
+      <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
+                  title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+
       <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
-          <i class="fas fa-euro-sign "></i>
+          {{-- ICON da notificação --}}
+          <i class="fas fa-birthday-cake"></i>
       </div>
 
       <div class="col p-2 ">
-          Existem pagamentos em atraso. <a href="#">Ver lista</a>
+          {{-- MENSAGEM da notificação --}}
+          Juan faz anos hoje! <a href="#">Enviar mensagem</a>
+      </div>
+  </div>
+  
+  
+
+
+
+
+
+    {{-- Divisão por data --}}
+    <div class="text-muted text-center m-2 mt-3" style="font-size:12px">8 de Março de 2020</div>
+
+
+
+
+    {{-- Exemplo de notificação --}}
+    <div class="alert-dismissible row p-1 mx-1 alert alert-secondary fade show">
+
+      {{-- Exemplo de "marcar como lido" --}}
+      <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
+                  title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+
+      <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
+          {{-- ICON da notificação --}}
+          <i class="fas fa-user-graduate"></i>
+      </div>
+
+      <div class="col p-2 ">
+          {{-- MENSAGEM da notificação --}}
+         Foi registado um novo aluno no sistema <a href="#">Ver ficha</a>
       </div>
   </div>
 
 
 
-
-  {{-- Exemplo de notificação --}}
-  <div class="row p-1 mx-1 alert alert-success">
-      <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
-          <i class="fas fa-university"></i>
-      </div>
-
-      <div class="col p-2 ">
-          UBI: Candidaturas abertas. <a href="#">Ver website</a>
-      </div>
-  </div>
+  {{-- FIM DAS NOTIFICAÇÕES --}}
 
 
 
-  {{-- Exemplo de notificação --}}
-  <div class="row p-1 mx-1 alert alert-info">
-    <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
-      <i class="fas fa-birthday-cake"></i>
+    <div class="mx-auto mb-3 pt-3 font-weight-bold text-center" style="color:#6A74C9;font-size:18px">
+        <div class="text-muted" style="font-size:12px"><a href="#">Ver tudo</a></div>
     </div>
-
-    <div class="col p-2 ">
-        Juan faz anos hoje<br><a href="#">Enviar mensagem</a>
-    </div>
-</div>    
-
-
-<div class="text-muted text-center m-2 mt-3" style="font-size:12px">8 de Março de 2020</div>  
-
-  {{-- Exemplo de notificação --}}
-  <div class="row p-1 mx-1 alert alert-secondary ">
-    <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
-      <i class="fas fa-user-graduate"></i>
-    </div>
-
-    <div class="col p-2 ">
-        Foi registado um novo aluno no sistema.<br><a href="#">Ver ficha</a>
-    </div>
-</div>  
-
-
-
-
-<div class="mx-auto mb-3 pt-3 font-weight-bold text-center" style="color:#6A74C9;font-size:18px">
-  <div class="text-muted" style="font-size:12px"><a href="#">Ver tudo</a></div>
 </div>
-</div>
-
-
-
