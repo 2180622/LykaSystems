9a3954edae201c5c3c07075ce6b6ea12469d3c04
Edgar Cordeiro <nill546@hotmail.com>
2020-03-09 12:51:35 +0000

    Inicio Models

--- app/Fase.php
+++ app/Fase.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class University extends Model
+class Fase extends Model
 {
-    //
+    protected $table = 'Fase';
 }
---
+++ app/PagoResponsabilidade.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PagoResponsabilidade extends Model
+{
+    protected $table = 'PagoResponsabilidade';
+}
