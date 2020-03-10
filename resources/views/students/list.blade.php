c2217d022a35eb360a5d3243414ad6680070ccb5
Micaela <2180482@my.ipleiria.pt>
2020-03-10 12:18:17 +0000

    Students

--- resources/views/students/list.blade.php
+++ resources/views/students/list.blade.php
@@ -11,75 +11,81 @@
 @endsection
 
 
-
-
-
 {{-- Conteudo da Página --}}
 @section('content')
-    <div class="container-fluid">
-        <h5>Lista de estudantes</h5>
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-6">
-                <div class="table-responsive">
-                    <table id="dataTable" class="table" style="width:100%; text-align:center">
-                        <thead>
-                        <tr>
-                            <th style="width:10%; padding-top: 20px;">
-                                <div class="fotoPerfil">
-                                    <div class="check">
-                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                        <label class="form-check-label" for="exampleCheck1"></label>
+    <div class="container mt-2">
+        <div class="float-right">
+            <button type="button" name="button" id="add-student">ADICIONAR CLIENTE</button>
+        </div>
+        <br>
+        <div class="cards-navigation">
+            <div class="title">
+                <h6>Listagem de estudantes</h6>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-6">
+                    <div class="table-responsive">
+                        <table id="dataTable" class="table" style="width:100%; text-align: center;">
+                            <thead>
+                            <tr>
+                                <th style="">
+                                    <div class="fotoPerfil">
+                                        <div class="check">
+                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                            <label class="form-check-label" for="exampleCheck1"></label>
+                                        </div>
                                     </div>
-                                </div>
-                            </th>
-                            <th style="text-align: left; padding-bottom: 20px">Nome de Utilizador</th>
-                            <th style="text-align: left; padding-bottom: 20px">Endereço Eletrónico</th>
-                            <th style="text-align: center; padding-bottom: 20px">Naturalidade</th>
-                            <th style="text-align: center; padding-bottom: 20px">Opções</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr class="bg-hover-row">
-                            <td>
-                                <div class="fotoPerfilImagem"></div>
-                            </td>
-                            <td style="text-align: left; padding-top:20px">Sebastian Sion</td>
-                            <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
-                            <td style="text-align: center; padding-top:20px">México</td>
-                            <td style="text-align: center; padding-top:20px">
-                                <i class="fas fa-ellipsis-h mr-3" title="Outros"></i>
-                                <i class="fas fa-pencil-alt mr-3" title="Editar"></i>
-                                <i class="far fa-trash-alt" title="Eliminar"></i>
-                            </td>
-                        </tr>
-                        <tr class="bg-hover-row">
-                            <td>
-                                <div class="fotoPerfilImagem"></div>
-                            </td>
-                            <td style="text-align: left; padding-top:20px">Sebastian Sion</td>
-                            <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
-                            <td style="text-align: center; padding-top:20px">México</td>
-                            <td style="text-align: center; padding-top:20px">
-                                <i class="fas fa-ellipsis-h mr-3" title="Outros"></i>
-                                <i class="fas fa-pencil-alt mr-3" title="Editar"></i>
-                                <i class="far fa-trash-alt" title="Eliminar"></i>
-                            </td>
-                        </tr>
-                        <tr class="bg-hover-row">
-                            <td>
-                                <div class="fotoPerfilImagem"></div>
-                            </td>
-                            <td style="text-align: left; padding-top:20px">Sebastian Sion</td>
-                            <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
-                            <td style="text-align: center; padding-top:20px">México</td>
-                            <td style="text-align: center; padding-top:20px">
-                                <i class="fas fa-ellipsis-h mr-3" title="Outros"></i>
-                                <i class="fas fa-pencil-alt mr-3" title="Editar"></i>
-                                <i class="far fa-trash-alt" title="Eliminar"></i>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                                </th>
+                                <th style="text-align: left; padding-bottom: 20px">Nome de Utilizador</th>
+                                <th style="text-align: left; padding-bottom: 20px">Endereço Eletrónico</th>
+                                <th style="text-align: center; padding-bottom: 20px">Naturalidade</th>
+                                <th style="text-align: center; padding-bottom: 20px">Opções</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr class="bg-hover-row">
+                                <td>
+                                    <div class="fotoPerfilImagem"></div>
+                                </td>
+                                <td style="text-align: left; padding-top:20px">Sebastian Sion</td>
+                                <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
+                                <td style="text-align: center; padding-top:20px">México</td>
+                                <td style="text-align: center; padding-top:20px">
+                                    <i class="fas fa-ellipsis-h mr-3" title="Outros"></i>
+                                    <i class="fas fa-pencil-alt mr-3" title="Editar"></i>
+                                    <i class="far fa-trash-alt" title="Eliminar"></i>
+                                </td>
+                            </tr>
+                            <tr class="bg-hover-row">
+                                <td>
+                                    <div class="fotoPerfilImagem"></div>
+                                </td>
+                                <td style="text-align: left; padding-top:20px">Sebastian Sion</td>
+                                <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
+                                <td style="text-align: center; padding-top:20px">México</td>
+                                <td style="text-align: center; padding-top:20px">
+                                    <i class="fas fa-ellipsis-h mr-3" title="Outros"></i>
+                                    <i class="fas fa-pencil-alt mr-3" title="Editar"></i>
+                                    <i class="far fa-trash-alt" title="Eliminar"></i>
+                                </td>
+                            </tr>
+                            <tr class="bg-hover-row">
+                                <td>
+                                    <div class="fotoPerfilImagem"></div>
+                                </td>
+                                <td style="text-align: left; padding-top:20px">Sebastian Sion</td>
+                                <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
+                                <td style="text-align: center; padding-top:20px">México</td>
+                                <td style="text-align: center; padding-top:20px">
+                                    <i class="fas fa-ellipsis-h mr-3" title="Outros"></i>
+                                    <i class="fas fa-pencil-alt mr-3" title="Editar"></i>
+                                    <i class="far fa-trash-alt" title="Eliminar"></i>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -90,4 +96,5 @@
 @section('scripts')
 
     <script src="{{asset('/js/student-list.js')}}"></script>
+
 @endsection
