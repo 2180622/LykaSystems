537ed4b3fa150dd9bf88ea4c2e4809b7c6ced9a1
Micaela <2180482@my.ipleiria.pt>
2020-03-10 15:15:30 +0000

    Style

--- resources/views/clients/list.blade.php
+++ resources/views/clients/list.blade.php
@@ -7,34 +7,32 @@
 
 {{-- Estilos de CSS --}}
 @section('styleLinks')
-<link href="{{asset('css/client.css')}}" rel="stylesheet">
+    <link href="{{asset('css/client.css')}}" rel="stylesheet">
 @endsection
 
 
 {{-- Conteudo da Página --}}
 @section('content')
-<div class="container mt-2">
-    <div class="float-right">
-        <a href="{{route('clients.create')}}" class="top-button">adicionar estudante</a>
-    </div>
-    <br>
-    <div class="cards-navigation">
-        <div class="title">
-            <h6>Listagem de estudantes</h6>
+    <div class="container mt-2">
+        <div class="float-right">
+            <a href="{{route('clients.create')}}" class="top-button">Adicionar Estudante</a>
         </div>
         <br>
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-6">
-                <div class="table-responsive">
-                    <table id="dataTable" class="table" style="width:100%; text-align: center;">
-                        <thead>
+        <div class="cards-navigation">
+            <div class="title">
+                <h6>Listagem de estudantes</h6>
+            </div>
+            <br>
+            <div class="row cards-group">
+                <div class="col-12">
+                    <div class="table-responsive">
+                        <table id="dataTable" class="table" style="width:100%; text-align: center;">
+                            <thead>
                             <tr>
-                                <th style="">
-                                    <div class="fotoPerfil">
-                                        <div class="check">
-                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                            <label class="form-check-label" for="exampleCheck1"></label>
-                                        </div>
+                                <th>
+                                    <div class="fotoPerfil check">
+                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                        <label class="form-check-label" for="exampleCheck1"></label>
                                     </div>
                                 </th>
                                 <th style="text-align: left; padding-bottom: 20px">Nome de Utilizador</th>
@@ -42,8 +40,8 @@
                                 <th style="text-align: center; padding-bottom: 20px">Naturalidade</th>
                                 <th style="text-align: center; padding-bottom: 20px">Opções</th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             <tr class="bg-hover-row">
                                 <td>
                                     <div class="fotoPerfilImagem"></div>
@@ -52,9 +50,12 @@
                                 <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
                                 <td style="text-align: center; padding-top:20px">México</td>
                                 <td style="text-align: center; padding-top:20px">
-                                    <a class="btnOption" href="#" title="Outros"> <i class="fas fa-ellipsis-h mr-3"></i> </a>
-                                    <a class="btnOption" href="#" title="Editar"> <i class="fas fa-pencil-alt mr-3"></i> </a>
-                                    <a class="btnOption" href="#" title="Eliminar"> <i class="far fa-trash-alt"></i> </a>
+                                    <a class="btnOption" href="#" title="Outros"> <i class="fas fa-ellipsis-h mr-3"></i>
+                                    </a>
+                                    <a class="btnOption" href="#" title="Editar"> <i class="fas fa-pencil-alt mr-3"></i>
+                                    </a>
+                                    <a class="btnOption" href="#" title="Eliminar"> <i class="far fa-trash-alt"></i>
+                                    </a>
                                 </td>
                             </tr>
                             <tr class="bg-hover-row">
@@ -65,9 +66,12 @@
                                 <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
                                 <td style="text-align: center; padding-top:20px">México</td>
                                 <td style="text-align: center; padding-top:20px">
-                                    <a class="btnOption" href="#" title="Outros"> <i class="fas fa-ellipsis-h mr-3"></i> </a>
-                                    <a class="btnOption" href="#" title="Editar"> <i class="fas fa-pencil-alt mr-3"></i> </a>
-                                    <a class="btnOption" href="#" title="Eliminar"> <i class="far fa-trash-alt"></i> </a>
+                                    <a class="btnOption" href="#" title="Outros"> <i class="fas fa-ellipsis-h mr-3"></i>
+                                    </a>
+                                    <a class="btnOption" href="#" title="Editar"> <i class="fas fa-pencil-alt mr-3"></i>
+                                    </a>
+                                    <a class="btnOption" href="#" title="Eliminar"> <i class="far fa-trash-alt"></i>
+                                    </a>
                                 </td>
                             </tr>
                             <tr class="bg-hover-row">
@@ -78,23 +82,26 @@
                                 <td style="text-align: left; padding-top:20px">sebastian@gmail.com</td>
                                 <td style="text-align: center; padding-top:20px">México</td>
                                 <td style="text-align: center; padding-top:20px">
-                                    <a class="btnOption" href="#" title="Outros"> <i class="fas fa-ellipsis-h mr-3"></i> </a>
-                                    <a class="btnOption" href="#" title="Editar"> <i class="fas fa-pencil-alt mr-3"></i> </a>
-                                    <a class="btnOption" href="#" title="Eliminar"> <i class="far fa-trash-alt"></i> </a>
+                                    <a class="btnOption" href="#" title="Outros"> <i class="fas fa-ellipsis-h mr-3"></i>
+                                    </a>
+                                    <a class="btnOption" href="#" title="Editar"> <i class="fas fa-pencil-alt mr-3"></i>
+                                    </a>
+                                    <a class="btnOption" href="#" title="Eliminar"> <i class="far fa-trash-alt"></i>
+                                    </a>
                                 </td>
                             </tr>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 
 {{-- Utilização de scripts: --}}
 @section('scripts')
 
-<script src="{{asset('/js/student-list.js')}}"></script>
+    <script src="{{asset('/js/student-list.js')}}"></script>
 
 @endsection
