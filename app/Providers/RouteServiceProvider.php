0b0768cbc3a05f3c4cddf0d10d0119d18093c702
José Areia <jose.apareia@gmail.com>
2020-03-11 09:41:00 +0000

    Routes - Authentication Middleware

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const LYKA = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
