3022fb889cfd2d3c91bdbcc0e15cd688a2cc9b20
José Areia <jose.apareia@gmail.com>
2020-03-12 14:44:15 +0000

    Login + Logout + Middleware

--- routes/web.php
+++ routes/web.php
@@ -2,32 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Contracts\Console\Kernel;
-use App\Mail\SendEmailConfirmation;
-use Illuminate\Support\Facades\Mail;
 
 Auth::routes();
 
 /* Route group protected with authentication */
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => ['auth', 'PreventBackHistory']], function () {
   /* Dashboard */
   Route::get('/', 'DashboardController@index')->name('dashboard');
 
   /* Phonebook */
   Route::resource('/phonebook', 'PhonebookController');
-});
 
-/* Utilizadores */
-Route::resource('/users', 'UserController');
+  /* Universidades */
+  Route::resource('/universities', 'UniversityController');
+
+  /* Estudantes */
+  Route::resource('/clients', 'ClientController');
+
+  /* Utilizadores */
+  Route::resource('/users', 'UserController');
+
+  /* Logout */
+  Route::get('logout', 'Auth\LoginController@logout');
+});
 
 /* Email Confirmation */
 Route::get('/confirmation/{user}', 'AccountConfirmationController@index')->name('confirmation.index');
 Route::post('/confirmation/{user}', 'AccountConfirmationController@update')->name('confirmation.update');
-
-/* Universidades */
-Route::resource('/universities', 'UniversityController');
-
-/* Estudantes */
-Route::resource('/clients', 'ClientController');
 
 /* Edgar Teste */ /* eliminar no futuro */
 Route::get('/edgarteste', 'EdgarTesteController@index');
