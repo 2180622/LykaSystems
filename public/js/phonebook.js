c8d2df8602f553e86bf6f52a69b2c7d4494f6f25
Daniel <daniel-nunes@live.com>
2020-03-18 19:18:53 +0000

    Contactos

---
+++ public/js/phonebook.js
@@ -0,0 +1,60 @@
+var table = $('#dataTable').DataTable({
+
+    "columnDefs": [{
+            "orderable": false,
+            "targets": 0
+        },
+        {
+            "orderable": false,
+            "targets": 2
+        },
+        {
+            "orderable": false,
+            "targets": 4
+        },
+        {
+            "width": "60px",
+            "targets": 0
+        },
+        {
+            "width": "auto",
+            "targets": 1
+        },
+        {
+            "width": "auto",
+            "targets": 2
+        },
+        {
+            "width": "auto",
+            "targets": 3
+        },
+        {
+            "width": "130px",
+            "targets": 4
+        },
+    ],
+
+
+    "language": {
+        "lengthMenu": "Mostrar _MENU_ por página",
+        "search": "Procurar",
+        "zeroRecords": "Sem registos",
+        "paginate": {
+            "first": "Primeiro",
+            "last": "Ultimo",
+            "next": "Proximo",
+            "previous": "Anterior"
+        },
+
+        "info": "",
+        "infoEmpty": "",
+        "infoFiltered": ""
+    },
+
+    "order": [1, 'desc'],
+
+    /* "bLengthChange": false, */
+    /* "bFilter": false, */
+
+
+});
