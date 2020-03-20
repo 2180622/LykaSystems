51b69adbb8470328bf6222a69a2c54b562ed13a6
José Areia <jose.apareia@gmail.com>
2020-03-20 19:22:24 +0000

    Sidebar style - *update and incomplete*

--- resources/views/layout/partials/sidebar.blade.php
+++ resources/views/layout/partials/sidebar.blade.php
@@ -1,22 +1,23 @@
-
-
-
 <div class="container-fluid " style="font-size:14px; height:70%">
 
-  <div class="mt-5 mx-auto mb-4 pt-3 font-weight-bold text-center" style="font-size:18px;color:#6A74C9">
-    <i class="fas fa-bell mr-2"></i>Notificações
-</div>
+    <div class="search-bar">
+        <p>Procure o que precise</p>
+        <div class="search-button">
+            <ion-icon name="search-outline" class="search-icon"></ion-icon>
+        </div>
+    </div>
 
-{{--     <div class="text-muted text-center m-2" style="font-size:12px">6 de Março de 2020</div> --}}
+    <div class="mt-5 mx-auto mb-4 pt-3 font-weight-bold" style="font-size:18px;">
+        Notificações
+    </div>
 
-
+    <div class="text-muted text-center m-2" style="font-size:12px">6 de Março de 2020</div>
 
     {{-- Exemplo de notificação --}}
     <div class="alert-dismissible row p-1 mx-1 alert alert-danger fade show">
 
         {{-- Exemplo de "marcar como lido" --}}
-        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
-                    title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i title="Marcar como lido" class="far fa-check-circle"></i></a></div>
 
         <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
             {{-- ICON da notificação --}}
@@ -38,8 +39,7 @@
     <div class="alert-dismissible row p-1 mx-1 alert alert-success fade show">
 
         {{-- Exemplo de "marcar como lido" --}}
-        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
-                    title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i title="Marcar como lido" class="far fa-check-circle"></i></a></div>
 
         <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
             {{-- ICON da notificação --}}
@@ -58,20 +58,19 @@
     {{-- Exemplo de notificação --}}
     <div class="alert-dismissible row p-1 mx-1 alert alert-info fade show">
 
-      {{-- Exemplo de "marcar como lido" --}}
-      <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
-                  title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+        {{-- Exemplo de "marcar como lido" --}}
+        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i title="Marcar como lido" class="far fa-check-circle"></i></a></div>
 
-      <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
-          {{-- ICON da notificação --}}
-          <i class="fas fa-birthday-cake"></i>
-      </div>
+        <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
+            {{-- ICON da notificação --}}
+            <i class="fas fa-birthday-cake"></i>
+        </div>
 
-      <div class="col p-2 ">
-          {{-- MENSAGEM da notificação --}}
-          Juan faz anos hoje! <br><a href="#">Enviar mensagem</a>
-      </div>
-  </div>
+        <div class="col p-2 ">
+            {{-- MENSAGEM da notificação --}}
+            Juan faz anos hoje! <br><a href="#">Enviar mensagem</a>
+        </div>
+    </div>
 
 
 
@@ -80,7 +79,7 @@
 
 
     {{-- Divisão por data --}}
-{{--     <div class="text-muted text-center m-2 mt-3" style="font-size:12px">8 de Março de 2020</div> --}}
+    {{-- <div class="text-muted text-center m-2 mt-3" style="font-size:12px">8 de Março de 2020</div> --}}
 
 
 
@@ -88,24 +87,23 @@
     {{-- Exemplo de notificação --}}
     <div class="alert-dismissible row p-1 mx-1 alert alert-secondary fade show">
 
-      {{-- Exemplo de "marcar como lido" --}}
-      <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i
-                  title="Marcar como lido" class="far fa-check-circle"></i></a></div>
+        {{-- Exemplo de "marcar como lido" --}}
+        <div class="close_btn text-center" style="font-size:14px;"><a href="#" data-dismiss="alert"><i title="Marcar como lido" class="far fa-check-circle"></i></a></div>
 
-      <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
-          {{-- ICON da notificação --}}
-          <i class="fas fa-user-graduate"></i>
-      </div>
+        <div class="col col-2 text-center mr-1 p-2 align-self-center" style="font-size:16px">
+            {{-- ICON da notificação --}}
+            <i class="fas fa-user-graduate"></i>
+        </div>
 
-      <div class="col p-2 ">
-          {{-- MENSAGEM da notificação --}}
-         Foi registado um novo aluno no sistema <br><a href="#">Ver ficha</a>
-      </div>
-  </div>
-
+        <div class="col p-2 ">
+            {{-- MENSAGEM da notificação --}}
+            Foi registado um novo aluno no sistema <br><a href="#">Ver ficha</a>
+        </div>
+    </div>
 
 
-  {{-- FIM DAS NOTIFICAÇÕES --}}
+
+    {{-- FIM DAS NOTIFICAÇÕES --}}
 
 
 
