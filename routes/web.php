52e7251862710dcaf15aead7baded92818ce38b2
Micaela <2180482@my.ipleiria.pt>
2020-03-25 11:04:07 +0000

    Library List

--- routes/web.php
+++ routes/web.php
@@ -7,31 +7,34 @@ Auth::routes();
 
 /* Route group protected with authentication */
 Route::group(['middleware' => ['auth', 'PreventBackHistory']], function () {
-  /* Dashboard */
-  Route::get('/', 'DashboardController@index')->name('dashboard');
+    /* Dashboard */
+    Route::get('/', 'DashboardController@index')->name('dashboard');
 
-  /* Report Problem */
-  Route::get('/reportproblem', 'DashboardController@report')->name('report');
+    /* Report Problem */
+    Route::get('/reportproblem', 'DashboardController@report')->name('report');
 
-  /* Phonebook */
-  Route::resource('/phonebook', 'PhonebookController');
+    /* Phonebook */
+    Route::resource('/phonebook', 'PhonebookController');
 
-  /* Universidades */
-  Route::resource('/universities', 'UniversityController');
+    /* Universidades */
+    Route::resource('/universities', 'UniversityController');
 
-  /* Estudantes */
-  Route::get('/clients/print/{client}', 'ClientController@print')->name('clients.print');
-  Route::resource('/clients', 'ClientController');
+    /* Estudantes */
+    Route::get('/clients/print/{client}', 'ClientController@print')->name('clients.print');
+    Route::resource('/clients', 'ClientController');
 
-  /* Agentes */
-  Route::resource('/agents', 'AgentController');
+    /* Agentes */
+    Route::resource('/agents', 'AgentController');
 
-  /* Logout */
-  Route::get('/logout', 'Auth\LoginController@logout');
+    /* Biblioteca */
+    Route::resource('/libraries', 'LibraryController');
 
-  /* Utilizadores */
-  Route::resource('/users', 'UserController');
-  Route::post('/users/storeAdmin', 'UserController@storeAdmin')->name('users.storeAdmin');
+    /* Logout */
+    Route::get('/logout', 'Auth\LoginController@logout');
+
+    /* Utilizadores */
+    Route::resource('/users', 'UserController');
+    Route::post('/users/storeAdmin', 'UserController@storeAdmin')->name('users.storeAdmin');
 });
 
 /* Email Confirmation */
