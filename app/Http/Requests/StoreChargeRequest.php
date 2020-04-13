e726782c9b154e00cd291f0a35c847dd1c9ae2ba
José Areia <jose.apareia@gmail.com>
2020-04-13 15:44:01 +0100

    Charge Request *update*

--- app/Http/Requests/StoreChargeRequest.php
+++ app/Http/Requests/StoreChargeRequest.php
@@ -26,8 +26,10 @@ class StoreChargeRequest extends FormRequest
     public function messages()
     {
       return[
-        'valorRecebido.required' => 'O campo Valor Recebido necessita de um valor.',
-        'valorRecebido.regex' => 'O campo Valor Recebido deve ter o seguinte formato: 10,00 ou 10.000.'
+        'valorRecebido.required' => 'O campo Valor Recebido necessita de ser preenchido.',
+        'valorRecebido.regex' => 'O campo Valor Recebido deve ter o seguinte formato: 10,00 ou 10.000.',
+        'dataOperacao.required' => 'O campo Data de Pagamento necessita de ser preenchido.',
+        'dataRecebido.required' => 'O campo Data de Receção necessita de ser preenchido.'
       ];
     }
 }
