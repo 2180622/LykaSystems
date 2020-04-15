f5599dbb43c7d85cd69ba8d3f55285d46a976453
Micaela <2180482@my.ipleiria.pt>
2020-04-15 10:56:46 +0100

    Main

--- resources/views/layout/partials/main-menu.blade.php
+++ resources/views/layout/partials/main-menu.blade.php
@@ -13,10 +13,12 @@
         <li class="menu-option">
             <a href="{{route('dashboard')}}">
                 <div class="menu-icon">
-                    <ion-icon name="cloud-outline" style="font-size: 16pt; position: relative; top: 3px; right: 3px; --ionicon-stroke-width: 40px;">
+                    <ion-icon name="cloud-outline"
+                              style="font-size: 16pt;  --ionicon-stroke-width: 40px; position: relative; top: 3px; right: 3px;">
                     </ion-icon>
                 </div>
-                <span class="{{Route::is('dashboard') ? 'active' : ''}} option-name" style="bottom:2px;">Dashboard</span>
+                <span class="{{Route::is('dashboard') ? 'active' : ''}} option-name"
+                      style="bottom:2px;">Dashboard</span>
             </a>
         </li>
 
@@ -26,7 +28,8 @@
         <li class="menu-option">
             <a href="{{route('clients.index')}}">
                 <div class="menu-icon">
-                    <ion-icon name="person-circle-outline" style="font-size: 16pt; position: relative; top: 5px; right: 3px;"></ion-icon>
+                    <ion-icon name="person-circle-outline"
+                              style="font-size: 16pt;  --ionicon-stroke-width: 40px; position: relative; top: 5px; right: 3px;"></ion-icon>
                 </div>
                 <span class="option-name {{Route::is('clients.*') ? 'active' : ''}} option-name">Estudantes</span>
             </a>
@@ -35,30 +38,30 @@
 
         <!-- Universidades  -->
         @if (Auth()->user()->tipo == 'admin')
-        <li class="menu-option">
-            <a href="{{route('universities.index')}}">
-                <div class="menu-icon">
-                    <i class="fas fa-university mr-2"></i>
-                </div>
-                <span class="option-name {{Route::is('universities.*') ? 'active' : ''}}">Universidades</span>
-            </a>
-        </li>
+            <li class="menu-option">
+                <a href="{{route('universities.index')}}">
+                    <div class="menu-icon">
+                        <i class="fas fa-university mr-2"></i>
+                    </div>
+                    <span class="option-name {{Route::is('universities.*') ? 'active' : ''}}">Universidades</span>
+                </a>
+            </li>
         @endif
 
 
 
 
-        <!-- Agentes  -->
+    <!-- Agentes  -->
         @if ( Auth::user()->tipo == "admin")
-        {{-- Só o admin tem acesso à lista --}}
-        <li class="menu-option">
-            <a href="{{route('agents.index')}}">
-                <div class="menu-icon">
-                    <i class="fas fa-user-tie mr-2"></i>
-                </div>
-                <span class="option-name {{Route::is('agents.*') ? 'active' : ''}}">Agentes</span>
-            </a>
-        </li>
+            {{-- Só o admin tem acesso à lista --}}
+            <li class="menu-option">
+                <a href="{{route('agents.index')}}">
+                    <div class="menu-icon">
+                        <i class="fas fa-user-tie mr-2"></i>
+                    </div>
+                    <span class="option-name {{Route::is('agents.*') ? 'active' : ''}}">Agentes</span>
+                </a>
+            </li>
         @endif
 
         <br>
@@ -68,7 +71,7 @@
                 <div class="menu-icon">
                     <i class="fas fa-tools mr-2"></i>
                 </div>
-                <span class="option-name">Diversos</span>
+                <span class="option-name">Diversos <i class="fas fa-caret-down"></i></span>
             </a>
         </li>
 
@@ -78,10 +81,11 @@
             <li class="menu-option">
                 <a href="{{route('payments.index')}}">
                     <div class="menu-icon">
-                        <ion-icon name="wallet-outline" style="font-size:16pt; --ionicon-stroke-width: 50px; position: relative; top: 3px; right: 3px;">
+                        <ion-icon name="cash-outline"
+                                  style="font-size:16pt; --ionicon-stroke-width: 40px; position: relative; top: 3px; right: 3px;">
                         </ion-icon>
                     </div>
-                    <span class="option-name">Pagamentos</span>
+                    <span class="option-name {{Route::is('payments.*') ? 'active' : ''}}">Pagamentos</span>
                 </a>
             </li>
 
@@ -89,10 +93,11 @@
             <li class="menu-option">
                 <a href="{{route('charges.index')}}">
                     <div class="menu-icon">
-                        <ion-icon name="wallet-outline" style="font-size:16pt; --ionicon-stroke-width: 50px; position: relative; top: 3px; right: 3px;">
+                        <ion-icon name="wallet-outline"
+                                  style="font-size:16pt; --ionicon-stroke-width: 40px; position: relative; top: 3px; right: 3px;">
                         </ion-icon>
                     </div>
-                    <span class="option-name">Cobranças</span>
+                    <span class="option-name {{Route::is('charges.*') ? 'active' : ''}}">Cobranças</span>
                 </a>
             </li>
 
@@ -123,7 +128,7 @@
                     <div class="menu-icon">
                         <i class="fas fa-phone-alt mr-2"></i>
                     </div>
-                    <span class="{{Route::is('contacts.*') ? 'active' : ''}} option-name">Lista telefónica</span>
+                    <span class="option-name {{Route::is('contacts.*') ? 'active' : ''}}">Lista telefónica</span>
                 </a>
             </li>
 
@@ -160,17 +165,16 @@
 
         <!-- Utilizadores -->
         @if (Auth()->user()->tipo == 'admin')
-        <li class="menu-option">
-            <a href="{{route('users.index')}}">
-                <div class="menu-icon">
-                    <i class="fas fa-users mr-2"></i>
-                </div>
-                <span class="{{Route::is('users.*') ? 'active' : ''}} option-name">Administradores</span>
-            </a>
-        </li>
+            <li class="menu-option">
+                <a href="{{route('users.index')}}">
+                    <div class="menu-icon">
+                        <i class="fas fa-users mr-2"></i>
+                    </div>
+                    <span class="{{Route::is('users.*') ? 'active' : ''}} option-name">Administradores</span>
+                </a>
+            </li>
         @endif
     </ul>
-
 
 
     <div class="text-center mb-4">
@@ -181,78 +185,80 @@
         </div>
 
         <div class="user_opts shadow-sm align-self-center">
-            <a href="#" title="Terminar sessão" class="user_btn" data-toggle="modal" data-target="#Modal"><i class="fas fa-power-off"></i></a>
+            <a href="#" title="Terminar sessão" class="user_btn" data-toggle="modal" data-target="#Modal"><i
+                    class="fas fa-power-off"></i></a>
             {{-- @csrf
             @method('POST') --}}
         </div>
         <!-- -->
 
 
-
         {{-- SE FOR ADMIN --}}
         @if (Auth::user()->tipo == "admin" && Auth::user()->idAdmin != null)
-        <div class="mx-auto user_photo rounded-circle shadow">
-            {{-- <a href="#" title="Ver as minhas informações"> --}}
-            {{-- Foto Utilizador --}}
-            @if(Auth::user()->admin->fotografia != null)
-                <img src="{{asset('/storage/admin-photos/'.Auth::user()->admin->fotografia)}}" style="width:100%">
+            <div class="mx-auto user_photo rounded-circle shadow">
+                {{-- <a href="#" title="Ver as minhas informações"> --}}
+                {{-- Foto Utilizador --}}
+                @if(Auth::user()->admin->fotografia != null)
+                    <img src="{{asset('/storage/admin-photos/'.Auth::user()->admin->fotografia)}}" style="width:100%">
                 @elseif (Auth::user()->admin->genero == "F")
-                <img src="{{asset('/storage/default-photos/F.jpg')}}" style="width:100%">
+                    <img src="{{asset('/storage/default-photos/F.jpg')}}" style="width:100%">
                 @else
-                <img src="{{asset('/storage/default-photos/M.jpg')}}" style="width:100%">
+                    <img src="{{asset('/storage/default-photos/M.jpg')}}" style="width:100%">
                 @endif
                 {{-- </a> --}}
-        </div>
+            </div>
 
-        <div class="text-center mt-3">
-            {{-- Nome e Perfil --}}
-            <span class="font-weight-bold text-uppercase">{{Auth::user()->admin->nome}}</span><br>
-            <span class="text-muted " style="font-size:14px">{{Auth::user()->tipo}}</span>
-        </div>
+            <div class="text-center mt-3">
+                {{-- Nome e Perfil --}}
+                <span class="font-weight-bold text-uppercase">{{Auth::user()->admin->nome}}</span><br>
+                <span class="text-muted " style="font-size:14px">{{Auth::user()->tipo}}</span>
+            </div>
 
 
-        {{-- SE FOR AGENTE / SUBAGENTE --}}
+            {{-- SE FOR AGENTE / SUBAGENTE --}}
         @elseif (Auth::user()->tipo == "agente" && Auth::user()->idAgente != null)
-        <div class="mx-auto user_photo rounded-circle shadow">
-            <a href="{{route('agents.show', Auth::user()->agente )}}" title="Ver as minhas informações">
-                {{-- Foto Utilizador --}}
-                @if(Auth::user()->agente->fotografia != null)
-                    <img src="{{asset('/storage/agent-photos/'.Auth::user()->agente->fotografia)}}" style="width:100%">
+            <div class="mx-auto user_photo rounded-circle shadow">
+                <a href="{{route('agents.show', Auth::user()->agente )}}" title="Ver as minhas informações">
+                    {{-- Foto Utilizador --}}
+                    @if(Auth::user()->agente->fotografia != null)
+                        <img src="{{asset('/storage/agent-photos/'.Auth::user()->agente->fotografia)}}"
+                             style="width:100%">
                     @elseif (Auth::user()->agente->genero == "F")
-                    <img src="{{asset('/storage/default-photos/F.jpg')}}" style="width:100%">
+                        <img src="{{asset('/storage/default-photos/F.jpg')}}" style="width:100%">
                     @else
-                    <img src="{{asset('/storage/default-photos/M.jpg')}}" style="width:100%">
+                        <img src="{{asset('/storage/default-photos/M.jpg')}}" style="width:100%">
                     @endif
-            </a>
-        </div>
+                </a>
+            </div>
 
-        <div class="text-center mt-3">
-            {{-- Nome e Perfil --}}
-            <span class="font-weight-bold text-uppercase">{{Auth::user()->agente->nome}}</span><br>
-            <span class="text-muted " style="font-size:14px">{{Auth::user()->agente->tipo}}</span>
-        </div>
+            <div class="text-center mt-3">
+                {{-- Nome e Perfil --}}
+                <span class="font-weight-bold text-uppercase">{{Auth::user()->agente->nome}}</span><br>
+                <span class="text-muted " style="font-size:14px">{{Auth::user()->agente->tipo}}</span>
+            </div>
 
 
-        {{-- SE FOR CLIENTE --}}
+            {{-- SE FOR CLIENTE --}}
         @elseif (Auth::user()->tipo == "cliente" && Auth::user()->idCliente != null)
-        <div class="mx-auto user_photo rounded-circle shadow">
-            <a href="#" title="Ver as minhas informações">
-                {{-- Foto Utilizador --}}
-                @if(Auth::user()->cliente->fotografia != null)
-                    <img src="{{asset('/storage/client-photos/'.Auth::user()->cliente->fotografia)}}" style="width:100%">
+            <div class="mx-auto user_photo rounded-circle shadow">
+                <a href="#" title="Ver as minhas informações">
+                    {{-- Foto Utilizador --}}
+                    @if(Auth::user()->cliente->fotografia != null)
+                        <img src="{{asset('/storage/client-photos/'.Auth::user()->cliente->fotografia)}}"
+                             style="width:100%">
                     @elseif (Auth::user()->cliente->genero == "F")
-                    <img src="{{asset('/storage/default-photos/F.jpg')}}" style="width:100%">
+                        <img src="{{asset('/storage/default-photos/F.jpg')}}" style="width:100%">
                     @else
-                    <img src="{{asset('/storage/default-photos/M.jpg')}}" style="width:100%">
+                        <img src="{{asset('/storage/default-photos/M.jpg')}}" style="width:100%">
                     @endif
-            </a>
-        </div>
+                </a>
+            </div>
 
-        <div class="text-center mt-3">
-            {{-- Nome e Perfil --}}
-            <span class="font-weight-bold text-uppercase">{{Auth::user()->cliente->nome}}</span><br>
-            <span class="text-muted " style="font-size:14px">{{Auth::user()->tipo}}</span>
-        </div>
+            <div class="text-center mt-3">
+                {{-- Nome e Perfil --}}
+                <span class="font-weight-bold text-uppercase">{{Auth::user()->cliente->nome}}</span><br>
+                <span class="text-muted " style="font-size:14px">{{Auth::user()->tipo}}</span>
+            </div>
         @endif
 
     </div>
