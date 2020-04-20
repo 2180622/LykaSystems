ca12e62dfe14aa4b21dbefc91e9fd6a09318f722
Micaela <2180482@my.ipleiria.pt>
2020-04-20 17:08:18 +0100

    Update inc.

---
+++ public/js/agends.js
@@ -0,0 +1,19 @@
+$(function () {
+    $(".saveEvent").click(function () {
+        let id = $("#modalCalendar input[name='id']").val();
+
+        let title = $("#modalCalendar input[name='titulo']").val();
+
+        let start = $("#modalCalendar input[name='dataInicio']").val();
+
+        let end = $("#modalCalendar input[name='dataFim']").val();
+
+        let color = $("#modalCalendar input[name='cor']").val();
+
+        let description = $("#modalCalendar input[name='descricao']").val();
+    });
+});
+
+function resetForm(form) {
+    $(form)[0].reset();
+}
