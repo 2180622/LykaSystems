06e686eb9971ac850e2c84a39e02f1b8e2a26745
Edgar <nill546@hotmail.com>
2020-04-21 23:27:16 +0100

    produtos com diferencas entre admin e agente

--- app/Agente.php
+++ app/Agente.php
@@ -17,7 +17,7 @@ class Agente extends Model
 
     protected $fillable = [
 
-        /* '$idAgenteAssociado' */'idAgenteAssociado','nome','apelido','genero','tipo','email','dataNasc','fotografia','morada','pais','NIF','num_doc','img_doc','info_doc','telefone1','telefone2'
+        'idAgenteAssociado','nome','apelido','genero','tipo','email','dataNasc','fotografia','morada','pais','NIF','num_doc','img_doc','info_doc','telefone1','telefone2'
         ];
 
     public function user(){
