9647d623ab8e8f1698ccfdd125dabdcb9882e2df
José Areia <jose.apareia@gmail.com>
2020-04-23 16:44:03 +0100

    Pagamentos + Filtragem [Atualização]

--- public/js/payments.js
+++ public/js/payments.js
@@ -18,3 +18,42 @@ function showFunnelIcon() {
     filterButton.style.float = "right";
     closeButton.style.display = "none";
 }
+
+// Escolha de filtragem
+var optionsDiv = document.getElementById("div-options");
+
+function estudante() {
+  var estudanteDiv = document.getElementById("div-estudante");
+  optionsDiv.style.display = "none";
+  estudanteDiv.style.display = "block";
+}
+
+function agente() {
+  var agenteDiv = document.getElementById("div-agente");
+  optionsDiv.style.display = "none";
+  agenteDiv.style.display = "block";
+}
+
+function subagente() {
+  var subagenteDiv = document.getElementById("div-subagente");
+  optionsDiv.style.display = "none";
+  subagenteDiv.style.display = "block";
+}
+
+function universidade() {
+  var universidadeDiv = document.getElementById("div-universidade");
+  optionsDiv.style.display = "none";
+  universidadeDiv.style.display = "block";
+}
+
+function fornecedor() {
+  var fornecedorDiv = document.getElementById("div-fornecedor");
+  optionsDiv.style.display = "none";
+  fornecedorDiv.style.display = "block";
+}
+
+function datas() {
+  var datasDiv = document.getElementById("div-data");
+  optionsDiv.style.display = "none";
+  datasDiv.style.display = "block";
+}
