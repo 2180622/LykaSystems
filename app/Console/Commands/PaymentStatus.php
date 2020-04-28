78a158cff3691b4fed5b3bc3e5b82fad17e3cd2e
José Areia <jose.apareia@gmail.com>
2020-04-28 16:00:24 +0100

    Command + Kernel [Atualização]

--- app/Console/Commands/PaymentStatus.php
+++ app/Console/Commands/PaymentStatus.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use Carbon\Carbon;
 use App\Responsabilidade;
 use Illuminate\Console\Command;
 
@@ -9,7 +10,7 @@ class PaymentStatus extends Command
 {
     protected $signature = 'payment:update';
 
-    protected $description = 'Update the payment status on DB';
+    protected $description = 'Update the payment status on database';
 
     public function __construct()
     {
@@ -18,6 +19,19 @@ class PaymentStatus extends Command
 
     public function handle()
     {
-      // Para cada responsabilidade verificar a sua data de vencimento
+      $responsabilidades = Responsabilidade::where('estado', '!=', 'Pago')->get();
+
+      foreach ($responsabilidades as $responsabilidade) {
+        $dataCliente = $responsabilidade->dataVencimentoPagamentoCliente;
+        $dataAgente = $responsabilidade->dataVencimentoPagamentoAgente;
+        $dataSubAgente = $responsabilidade->dataVencimentoPagamentoSubAgente;
+        $dataUni1 = $responsabilidade->dataVencimentoPagamentoUni1;
+        $dataUni2 = $responsabilidade->dataVencimentoPagamentoUni2;
+
+        if ($dataCliente < Carbon::now() || $dataAgente < Carbon::now() || $dataSubAgente < Carbon::now() || $dataUni1 < Carbon::now() || $dataUni2 < Carbon::now()) {
+          Responsabilidade::where('idResponsabilidade', $responsabilidade->idResponsabilidade)
+          ->update(['estado' => 'Dívida']);
+        }
+      }
     }
 }
