307e4afe5598c67d28549720f329948288daa2a9
José Areia <jose.apareia@gmail.com>
2020-05-05 08:00:21 +0100

    Ativação da conta [Atualização]

--- app/Http/Controllers/AccountConfirmationController.php
+++ app/Http/Controllers/AccountConfirmationController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use App\Http\Controllers\UserController;
-use App\Http\Requests\UpdateUserRequest;
+use Illuminate\Support\Facades\Auth;
 
 class AccountConfirmationController extends Controller
 {
     public function index(Request $request, User $user)
     {
-      return view('auth.confirmation-key', compact('user'));
+      if ($user->estado == true) {
+          abort(404);
+      }else {
+          return view('auth.confirmation-key', compact('user'));
+      }
     }
 
     public function keyconfirmation(Request $request, User $user)
     {
-      $auth_key = $request->input('key');
+      $auth_key = $request->only('key');
 
-      if ($user->auth_key == $auth_key) {
+      if ($user->auth_key == $auth_key['key']) {
           return view('auth.confirmation-password', compact('user'));
       }else {
           $error = "O código de autenticação que introduziu é inválido.";
@@ -35,15 +38,13 @@ class AccountConfirmationController extends Controller
         $hashed = Hash::make($password);
         $user->password = $hashed;
         $user->save();
+        if (Auth::check()) {
+          Auth::logout();
+        }
         return view('auth.accountactive', compact('user'));
       }else {
         $error = "As palavras-chaves não coincidem. Verifique a sua inserção.";
         return view('auth.confirmation-password', compact('user', 'error'));
       }
-    }
-
-    public function setpassword(Request $request, User $user)
-    {
-
     }
 }
