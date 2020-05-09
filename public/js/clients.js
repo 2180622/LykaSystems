82c92ed20bf3de07a25b82253af5f83e8c897e39
Daniel <daniel-nunes@live.com>
2020-05-09 22:05:14 +0100

    asdad

--- public/js/clients.js
+++ public/js/clients.js
@@ -151,34 +151,34 @@
 
         //Preview do Passaporte +++++++++++++++
 
-/*         $('#passport_preview_file').on('click', function (e) {
-            e.preventDefault();
-            $('#img_Passaporte').trigger('click');
-        });
+        /*         $('#passport_preview_file').on('click', function (e) {
+                    e.preventDefault();
+                    $('#img_Passaporte').trigger('click');
+                });
 
-        $('#passport_preview').on('click', function (e) {
-            e.preventDefault();
-            $('#img_Passaporte').trigger('click');
-        });
+                $('#passport_preview').on('click', function (e) {
+                    e.preventDefault();
+                    $('#img_Passaporte').trigger('click');
+                });
 
-        function readPassaporteURL(input) {
-            if (input.files && input.files[0]) {
-                var passaporte = new FileReader();
+                function readPassaporteURL(input) {
+                    if (input.files && input.files[0]) {
+                        var passaporte = new FileReader();
 
-                passaporte.onload = function (e) {
-                    $('#name_passaporte_file').text(input.files[0].name);
+                        passaporte.onload = function (e) {
+                            $('#name_passaporte_file').text(input.files[0].name);
+                        }
+
+                        passaporte.readAsDataURL(input.files[0]);
+                    }
                 }
 
-                passaporte.readAsDataURL(input.files[0]);
-            }
-        }
+                $("#img_Passaporte").change(function () {
+                    readPassaporteURL(this);
+                    $('#passport_preview_file').hide();
+                    $('#passport_preview').show();
 
-        $("#img_Passaporte").change(function () {
-            readPassaporteURL(this);
-            $('#passport_preview_file').hide();
-            $('#passport_preview').show();
-
-        }); */
+                }); */
 
 
 
@@ -226,49 +226,129 @@
             $("#telefone2").numbersOnly();
             $("#telefonePai").numbersOnly();
             $("#telefoneMae").numbersOnly();
-/*             $("#num_docOficial").numbersOnly();
-            $("#numPassaporte").numbersOnly();
-            $("#IBAN").numbersOnly(); */
+            /*             $("#num_docOficial").numbersOnly();
+                        $("#numPassaporte").numbersOnly();
+                        $("#IBAN").numbersOnly(); */
             $("#NIF").numbersOnly();
 
         }
 
 
         /* VALIDAÇÃO DO FORMULÁRIO */
-        (function() {
+
+        // Example starter JavaScript for disabling form submissions if there are invalid fields
+        (function () {
             'use strict';
-            window.addEventListener('load', function() {
-              // Fetch all the forms we want to apply custom Bootstrap validation styles to
-              var forms = document.getElementsByClassName('needs-validation');
-              // Loop over them and prevent submission
-              var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
+            window.addEventListener('load', function () {
+                // Fetch all the forms we want to apply custom Bootstrap validation styles to
+                var forms = document.getElementsByClassName('needs-validation');
+                // Loop over them and prevent submission
+                var validation = Array.prototype.filter.call(forms, function (form) {
+                    form.addEventListener('submit', function (event) {
 
                         /* mostrar div de espera */
                         $("#wait_screen").show();
 
-                  if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
 
 
-                            alert("");
-                         /* valida Campos da informação pessoal */
-                         if (($("#nome").val() == "") || ($("#apelido").val() == "") || ($("#genero").val() == "") || ($("#paisNaturalidade").val() == "") || ($("#dataNasc").val() == "") ) {
-                            $("#wait_screen").hide();
-                            $("#pessoal-tab").addClass("border-danger text-danger");
-                            $("#warning_msg").show();
-                        } else {
-                            $("#pessoal-tab").removeClass("border-danger text-danger");
+
+
+                        if (form.checkValidity() === false) {
+                            event.preventDefault();
+                            event.stopPropagation();
+
+
+
+                            /* valida Campos da informação pessoal */
+                            if (($("#nome").val() == "") || ($("#apelido").val() == "") || ($("#genero").val() == "") || ($("#paisNaturalidade").val() == "") || ($("#dataNasc").val() == "")) {
+                                $("#wait_screen").hide();
+                                $("#pessoal-tab").addClass("border-danger text-danger");
+                                $("#warning_msg").show();
+                            } else {
+                                $("#pessoal-tab").removeClass("border-danger text-danger");
+                            }
+
+
+                            /* valida Campos dos documentos */
+                            if (($("#num_docOficial").val() == "") || ($("#num_passaporte").val() == "")) {
+                                $("#wait_screen").hide();
+                                $("#documentation-tab").addClass("border-danger text-danger");
+                                $("#warning_msg").show();
+                            } else {
+                                $("#documentation-tab").removeClass("border-danger text-danger");
+
+                            }
+
+                            /* valida Campos dos dados académicos */
+                            /*if (($("#nivEstudoAtual").val() == "") ) {
+                                $("#wait_screen").hide();
+                                $("#academicos-tab").addClass("border-danger text-danger");
+                                $("#warning_msg").show();
+                            } else {
+                                $("#academicos-tab").removeClass("border-danger text-danger");
+                            } */
+
+
+
+                            /* valida Campos dos contactos */
+                            if (($("#telefone1").val() == "") || ($("#email").val() == "")) {
+                                $("#wait_screen").hide();
+                                $("#contacts-tab").addClass("border-danger text-danger");
+                                $("#warning_msg").show();
+                            } else {
+                                $("#contacts-tab").removeClass("border-danger text-danger");
+
+                            }
+
+
+                            /* valida Campos das moradas */
+                            /*if (($("#moradaResidencia").val() == "") || ($("#morada").val() == "") || ($("#cidade").val() == "")) {
+                                $("#wait_screen").hide();
+                                $("#contacts-tab").addClass("border-danger text-danger");
+                                $("#warning_msg").show();
+                            } else {
+                                $("#contacts-tab").removeClass("border-danger text-danger");
+                            } */
+
+
+
+                            /* valida Campos das finanças */
+                            /*if ($("#IBAN").val() == "") {
+                                $("#wait_screen").hide();
+                                $("#financas-tab").addClass("border-danger text-danger");
+                                $("#warning_msg").show();
+                            } else {
+                                $("#financas-tab").removeClass("border-danger text-danger");
+                            } */
+
+                            /* Valida se tem agente associado */
+
+                            if ( $('#idAgente').val() == "0"){
+                                $("#wait_screen").hide();
+                                $("#div_agente").addClass("border border-danger text-danger ");
+                                $("#idAgente").removeClass("is-valid");
+                                $("#idAgente").addClass("is-invalid");
+                                $("#idAgente").css("background-image", "none");
+                                $("#warning_msg").show();
+
+
+                            }else{
+                                $("#div_agente").removeClass("border-danger text-danger");
+                            }
+
+
+
                         }
-
-
-                  }
-                  form.classList.add('was-validated');
-                }, false);
-              });
+                        window.scrollTo(0, 0);
+                        form.classList.add('was-validated');
+                    }, false);
+                });
             }, false);
-          })();
+        })();
+
+
+
+
 
 
 
