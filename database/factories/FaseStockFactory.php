033999534c4f4e10b83424ebc58d38f16f766247
Edgar <nill546@hotmail.com>
2020-05-18 17:10:20 +0100

    Factory Test [a funcionar]

--- database/factories/FaseStockFactory.php
+++ database/factories/FaseStockFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(FaseStock::class, function (Faker $faker) {
     return [
         'descricao' => $faker->randomElement($array = array ('Inscricao','Matricula', 'Final')),
-
+        
         /*  'idProdutoStock'  */
     ];
 });
