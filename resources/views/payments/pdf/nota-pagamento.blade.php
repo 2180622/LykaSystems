3285016c10c21174d9235c35c3e85e77dfe8ec92
José Areia <jose.apareia@gmail.com>
2020-05-18 19:23:34 +0100

    Pagamentos [PDF - Em curso]

--- resources/views/payments/pdf/nota-pagamento.blade.php
+++ resources/views/payments/pdf/nota-pagamento.blade.php
@@ -4,17 +4,105 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <title>Nota de pagamento - Lyka Systems</title>
+    <link href="{{asset('/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 </head>
 
 <style media="screen">
+    body {
+        padding: 25px 30px;
+    }
+
     img {
         width: 130px;
+    }
+
+    #text-beneficiario {
+        font-size: 10pt;
+        color: #5B5B5B;
+    }
+
+    #nome {
+        color: #171717 !important;
+        font-size: 11pt;
+        font-weight: bold;
+    }
+
+    table {
+        width: 100%;
+        color: #000;
+    }
+
+    table thead tr th {
+        text-transform: uppercase !important;
+        font-size: 11pt;
+    }
+
+    table thead th {
+        padding: 10px 5px !important;
+        border-bottom: 2px black solid;
     }
 </style>
 
 <body>
-    <img src="{{asset('/media/logo.png')}}" alt="Logótipo - Estudar Portugal">
-    <p>{{$responsabilidade->cliente->nome.' '.$responsabilidade->cliente->apelido}}</p>
+    <div class="header row">
+        <div class="col-md-6">
+            <img src="{{asset('/media/logo.png')}}" alt="Logótipo - Estudar Portugal">
+        </div>
+        <div class="col-md-6">
+            <div class="text-right" id="text-beneficiario">
+                <p class="mb-1" id="nome">Tiago Oliveira</p>
+                <p class="mb-0">Rua das Oliveiras Verdes</p>
+                <p class="mb-0">Edifício Amarelo, Nº13</p>
+                <p class="mb-0">3100-231 - Leiria</p>
+            </div>
+        </div>
+    </div>
+
+    <table class="mt-3">
+        <thead>
+            <tr>
+                <th id="hey">Descrição do pagamento</th>
+                <th>Data de pagamento</th>
+                <th>Valor</th>
+            </tr>
+        </thead>
+        <tr>
+            <td>Propinas</td>
+            <td>15/05/2020</td>
+            <td>500,00€</td>
+        </tr>
+        <tr>
+            <td>Matrícula</td>
+            <td>12/05/2020</td>
+            <td>75,00€</td>
+        </tr>
+        <tr>
+            <td>Outras importâncias</td>
+            <td>02/05/2020</td>
+            <td>625,00€</td>
+        </tr>
+    </table>
+
+    <div class="info mt-5">
+        <div>
+            <p class="font-weight-bold">Informações</p>
+            <p class="mb-1">Pagamento: 236584</p>
+            <p class="mb-1">Data de emissão: 16/05/2020</p>
+        </div>
+        <br>
+        <div>
+            <p class="font-weight-bold">Contactos</p>
+            <p class="mb-1">Rua de Leiria, 3000-241, Leiria</p>
+            <p class="mb-1">+351 244 523 698 | estudarportugal
+                @gmail.com</p>
+                <p class="mb-1">www.estudarportugal.com</p>
+        </div>
+        <br>
+        <div>
+            <p class="font-weight-bold">Termos e condições</p>
+            <p>Aque repellat quo cum tenetur sit ullam ut dolore impedit sint non.</p>
+        </div>
+    </div>
 </body>
 
 </html>
